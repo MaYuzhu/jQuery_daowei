812ba60e46941396566bb6efaab286f03fb2c702
MaYuzhu <mayuzhu123@sina.com>
2018-03-15 16:54:24 +0800

    第2天

--- js/index.js
+++ js/index.js
@@ -4,10 +4,8 @@ $(function () {
   	this.style.transition = 0.3 + 's'
     this.className = 'on'
   },function () {
-
 	  this.className = ''
-	  //transition 0.3s
-	  //color red
+
   })
 
 	//轮播左侧导航ul移入改变
@@ -27,6 +25,18 @@ $(function () {
 		this.className = ''
 	})
 
+	//头部动
+	$(document).scroll(function () {
+		//console.log($(window).scrollTop())
+		if($(window).scrollTop()>80){
+			$('.header').prop('class','header showhead')
+		}else{
+			$('.header').prop('class','header')
+		}
 
+	});
+  $('.header').on('click',function () {
+	  $('.header').slideUp();
+  })
 
 })
